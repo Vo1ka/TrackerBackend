80dcf297fa8072f8c9a302a6f8f8b23e2faa7f62
Vo1ka <syhov2015@ya.ru>
2025-07-08 15:10:00 +0300

    change url fron x2

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5173', 
       'http://localhost:3000', 
-      'https://stay-mot-beta-hnux0ycn2-vo1kas-projects.vercel.app/'], // сюда добавь адреса фронта, с которых будут запросы
+      'https://stay-mot-beta.vercel.app/'], // сюда добавь адреса фронта, с которых будут запросы
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // если используешь куки или авторизацию с credentials
