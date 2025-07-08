3b61cf378e328237820ddef89b03e2e8c01422c4
Vo1ka <syhov2015@ya.ru>
2025-07-08 14:07:14 +0300

    added url front

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ async function bootstrap() {
   });
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'], // сюда добавь адреса фронта, с которых будут запросы
+    origin: ['http://localhost:5173', 
+      'http://localhost:3000', 
+      'https://stay-mot-beta-hnux0ycn2-vo1kas-projects.vercel.app/'], // сюда добавь адреса фронта, с которых будут запросы
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // если используешь куки или авторизацию с credentials
